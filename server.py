315cc183e8ceaa6c33eb208fd235041a1d2f6ec2
Artur Renault <artur.renault@gmail.com>
2014-09-14 22:15:23 -0400

    Server accept connections

--- server.py
+++ server.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python
+import socket
 import string
+import sys
 
+try:
+    port = int(sys.argv[1])
+except (IndexError, ValueError):
+    sys.exit("usage: ./server.py <port>")
+
+# Create dictionary of usernames and passwords
 passwords = dict()
-user_file = open("user_pass.txt")
+try:
+    user_file = open("user_pass.txt")
+except IOError:
+    sys.exit("\"user_pass.txt\" not found.")
 for line in user_file:
     user = string.split(line)
     passwords[user[0]] = user[1]
+
+# Create socket
+# No parameters needed because Python defaults to
+# AF_INET and SOCK_STREAM, which are what we want.
+serv_sock = socket.socket()
+
+# Make reusing addresses possible
+serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind to local host with specified port
+serv_sock.bind(("", port))
+
+# Set socket to listen
+status = serv_sock.listen(5)
+print("Chat server is listening on port " + str(port))
+while True:
+    (clnt_sock, clnt_addr) = serv_sock.accept()
+    print("Connected to " + str(clnt_addr))
+    clnt_sock.close()
